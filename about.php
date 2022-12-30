797850447252152711c3dfb8d93f3fcdc018f8cc
Radisami <121139549+Radisami@users.noreply.github.com>
2022-12-30 21:04:23 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title> login</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css\style.css">
+    </head>
+<body>
+    
+    <nav class="menu">
+     <ul>
+      <li><a href="http://localhost/market/radi.php">Home</a></li>    
+      <li><a href="http://localhost/market/gallery.php">Gallery</a></li> 
+      <li><a href="http://localhost/market/help.php">Help</a></li>
+      <li><a href="http://localhost/market/about.php">About</a></li>
+      <li><a href="http://localhost/market/COURS1.php">Sign in</a></li>
+     </ul>
+    
+ <form class="search-form">
+     
+     <input type="text" placeholder="search">
+     <button>search</button>
+     
+ </form>
+       <img style="width: 100px;float: right;margin-right: 20px;" src="img/rg.png"  > 
+    </nav>
+     
+    <div class="box">
+        
+        <h2>about</h2>
+        <form>
+          <h5 style="color:white;">
+              amazon is website store to buy clothes online at a good price. 
+          </h5>  
+          <h5 style="color:white;margin-left: 80px;">amazon shopping fun</h5>    
+</form>
+        
+    </div>
+
+    <div class="footer">
+      <table style="margin-left: 580px;">
+          <tr>
+              <td><a href="#"><h4 style="color: white;">call as   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">politics   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">privacy     </h4></a></td>
+          </tr>
+      </table>  
+      <p style="margin-left:603px;font-size: 10px;">جميع الحقوق محفوظة للدفعة 11 شبكات 2022</p>
+    </div>
+    
+    
+</body>    
+</html>
